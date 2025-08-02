efc5f5f0f5643599038809389bf40783707b99b5
chvkrsubhash <141931134+chvkrsubhash@users.noreply.github.com>
2025-08-02 10:35:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+from flask import Flask, jsonify
 import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "✅ Flask App is Running!"
 
 @app.route("/get-my-ip")
 def get_my_ip():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     return jsonify({"internal_ip": ip_address})
+
+if __name__ == "__main__":
+    app.run()
